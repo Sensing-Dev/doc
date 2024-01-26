985843e2181bd942573dc509a7873fcacd08ba29
Fixstars-momoko <momoko.kono@fixstars.com>
2024-01-26 13:44:43 -0800

    added the static const

---
+++ static/config.js
@@ -0,0 +1,10 @@
+const latest_version = 'v24.01.01';
+const latest_URL = 'https://github.com/Sensing-Dev/sensing-dev-installer/releases/download/' + latest_version;
+const latest_installer_URL = latest_URL + '/installer.ps1';
+const latest_winUSB_installer_URL = latest_URL + '/installer-WinUSB.ps1';
+
+module.exports = {
+    latest_version,
+    latest_installer_URL,
+    latest_winUSB_installer_URL
+};
