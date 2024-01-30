3d582a45e36b35f336e6c7ba9efec55d9a26bf92
Xinyu Li <xinyuli@xinyus-air.lan>
2024-01-30 12:52:16 -0800

    add

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,15 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
+  plugins: [
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        /* options */
+      },
+    ],
+  ],
+
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
