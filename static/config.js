e3cd481068ef98d8acb45e3dd541d69625dedffc
Fixstars-momoko <momoko.kono@fixstars.com>
2024-02-10 11:56:24 -0800

    updated winusb installer generator

--- static/config.js
+++ static/config.js
@@ -3,7 +3,7 @@ const latest_URL = 'https://github.com/Sensing-Dev/sensing-dev-installer/release
 const latest_installer_URL = latest_URL + '/installer.ps1';
 
 // WinUSB
-const latest_winUSB_URL = "v24.02.00"
+const latest_winUSB_URL = "v24.02.01"
 const one_line_powershell = "Invoke-WebRequest -Uri https://github.com/Sensing-Dev/WinUSB-installer-generator/releases/download/"+latest_winUSB_URL+"/generate_and_apply_WinUSB.ps1 -OutFile generate_and_apply_WinUSB.ps1 -Verbose; powershell.exe -ExecutionPolicy Bypass -File ./generate_and_apply_WinUSB.ps1"
 
 module.exports = {
