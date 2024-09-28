68e0898179a84d6242d6fa772f325091fdb87030
Fixstars-momoko <momoko.kono@fixstars.com>
2024-09-27 18:15:45 -0700

    updated the latest version to v24.09.02

--- static/version_const/latest.js
+++ static/version_const/latest.js
@@ -1,6 +1,6 @@
 //THIS IS WHERE YOU UPDATE
 const is_latest = true;
-const latest_version = 'v24.09.01';
+const latest_version = 'v24.09.02';
 const latest_winUSB_URL = "v24.02.02"
 const ion_python_version = '3.2.0'
 const gendc_python_version = '0.3.3'
