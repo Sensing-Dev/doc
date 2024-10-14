25a9cee65ded6108c1d7e50a61231c86092ff92e
Fixstars-momoko <momoko.kono@fixstars.com>
2024-10-14 12:59:31 -0700

    fixed the baseUrl after removing post-process

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://${organizationName}.github.io ',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
