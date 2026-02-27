829d9003eef7fd97fc980464de418729d730dd96
Kono Momoko (Fixstars Solutions, Inc. momoko.kono) <momoko.kono@fixstars.com>
2026-02-27 10:23:06 -0800

    updated the latest version of sdk by ion-kit update

--- static/version_const/latest.js
+++ static/version_const/latest.js
@@ -1,8 +1,8 @@
 //THIS IS WHERE YOU UPDATE
 const is_latest = true;
-const latest_version = 'v25.01.00';
+const latest_version = 'v25.01.03';
 const latest_winUSB_URL = "v24.02.02"
-const ion_python_version = '3.2.6'
+const ion_python_version = '3.2.8'
 const gendc_python_version = '0.4.0'
 const aravis_python_version = '0.8.31.dev1'
 
